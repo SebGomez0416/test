e2329fe259c56f3627a2b8d0878f64ca256f2dfc
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-04-09 08:13:18 -0300

    Add move character

---
+++ test/Assets/Scripts/Character/move.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private CharacterController cc;
+    private Vector3 movement;
+    [SerializeField] private float speed;
+    
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal")* speed;
+        movement.z = Input.GetAxisRaw("Vertical")* speed;
+        
+        cc.SimpleMove(movement);
+    }
+}
