03ac81baba2507fabf08521eb3eb7bc87e92f1fd
juan sebastian gomez <sebbastian.gc@gmail.com>
2022-04-11 06:42:08 -0300

    Add Stage Four

--- test/Assets/Scripts/Character/view.cs
+++ test/Assets/Scripts/Character/view.cs
@@ -1,14 +1,23 @@
+using System;
 using UnityEngine;
 public class view : MonoBehaviour
 {
     [SerializeField]private float distance;
     [SerializeField] private LayerMask collider;
     private ChangeView roof;
+    [SerializeField]private ChangeView[] roofs;
+    
+    private void Start()
+    {
+        roofs = new ChangeView[3];
+    }
+
     private void Update()
     {
         Debug.DrawRay(transform.position,transform.up* distance, Color.red);
-
-        RaycastHit hit;
+        
+        //hard
+        /*RaycastHit hit;
         if (Physics.Raycast(transform.position,Vector3.up,out hit,distance,collider))
         { 
             // easy
@@ -22,6 +31,30 @@ public class view : MonoBehaviour
         {
            if (roof == null) return;
            roof.ViewRoof();
+        }*/
+        
+        //Nightmare  // 
+        
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, Vector3.up, distance, collider);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            roofs[i]= hits[i].transform.gameObject.GetComponent<ChangeView>();
+            roofs[i].HideRoof();
         }
+
+
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].transform.gameObject.CompareTag("roof") )
+                roofs[i].ViewRoof();
+
+        }
+           
+            
+        // tema de rendimientno hacer getcomponet en update 
+        //tema  de cambio de color "corrutinas "
     }
 }
